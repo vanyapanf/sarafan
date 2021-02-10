2f6e61d631b8c6cdeee76b5b5d38d04f98857ec0
Ivan Panfilov <ivan.panf@yandex.ru>
2021-02-10 23:15:49 +0300

    add websockets

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
+import { connect } from 'util/ws.js'
+
+if (frontendData.profile) {
+    connect();
+}
 
 Vue.use(VueResource)
 
